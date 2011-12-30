616c901d02c74ee67a97ff808e387e7fe84d0b49
Chris Mattmann <chris.mattmann@gmail.com>
2011-12-30 06:43:25 +0000

    Change namespace to org.apacheextras per COMDEV-65 and advice from Apache ComDev.

--- core/src/main/java/org/apacheextras/oodt/cas/pushpull/protocol/ftp/JvFtpClient.java
+++ core/src/main/java/org/apacheextras/oodt/cas/pushpull/protocol/ftp/JvFtpClient.java
@@ -15,7 +15,7 @@
  * limitations under the License.
  */
 
-package org.apache.oodt.cas.pushpull.protocol.ftp;
+package org.apacheextras.oodt.cas.pushpull.protocol.ftp;
 
 //JDK imports
 import java.io.File;
