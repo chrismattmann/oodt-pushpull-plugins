1ada17f967975dbb25bec4be5fa1176621960a5a
chris.mattmann@gmail.com <chris.mattmann@gmail.com@6142175d-7dc7-2ebc-9cbc-572fe326e10b>
2012-07-21 19:55:49 +0000

    Updates to Ftp4Che protocol to work with 0.4 protocol API.

--- trunk/core/src/main/java/org/apacheextras/oodt/cas/pushpull/protocol/ftp/Ftp4CheFtpClient.java
+++ trunk/core/src/main/java/org/apacheextras/oodt/cas/pushpull/protocol/ftp/Ftp4CheFtpClient.java
@@ -28,10 +28,11 @@ import org.ftp4che.FTPConnectionFactory;
 import org.ftp4che.util.ftpfile.FTPFile;
 
 //OODT imports
-import org.apache.oodt.cas.pushpull.exceptions.ProtocolException;
-import org.apache.oodt.cas.pushpull.protocol.Protocol;
-import org.apache.oodt.cas.pushpull.protocol.ProtocolFile;
-import org.apache.oodt.cas.pushpull.protocol.ProtocolPath;
+import org.apache.oodt.cas.protocol.exceptions.ProtocolException;
+import org.apache.oodt.cas.protocol.Protocol;
+import org.apache.oodt.cas.protocol.ProtocolFile;
+import org.apache.oodt.cas.protocol.auth.Authentication;
+import org.apache.oodt.cas.protocol.util.ProtocolFileFilter;
 
 /**
  * 
@@ -42,28 +43,32 @@ import org.apache.oodt.cas.pushpull.protocol.ProtocolPath;
  * Describe your class here
  * </p>.
  */
-public class Ftp4CheFtpClient extends Protocol {
+public class Ftp4CheFtpClient implements Protocol {
 
     FTPConnection ftp;
 
-    protected Ftp4CheFtpClient() {
-        super("ftp");
-    }
-
     @Override
-    protected void chDir(ProtocolPath path) throws ProtocolException {
+    public void cd(ProtocolFile file) throws ProtocolException {
         try {
-            ftp.changeDirectory(path.getPathString());
+            ftp.changeDirectory(file.getPath());
         } catch (Exception e) {
-            throw new ProtocolException("Failed to cd to " + path + " : "
+            throw new ProtocolException("Failed to cd to " + file.getPath() + " : "
                     + e.getMessage());
         }
     }
 
+
+
     @Override
-    public void cdToRoot() throws ProtocolException {
+    public void cdHome() throws ProtocolException{
+	cd(new ProtocolFile(ProtocolFile.SEPARATOR, true));
+    }
+
+
+    @Override
+    public void cdRoot() throws ProtocolException {
         try {
-            chDir(new ProtocolPath("/", true));
+            cd(new ProtocolFile("/", true));
         } catch (Exception e) {
             throw new ProtocolException("Failed to cd to root : "
                     + e.getMessage());
@@ -71,11 +76,11 @@ public class Ftp4CheFtpClient extends Protocol {
     }
 
     @Override
-    protected void connect(String host, String username, String password)
+	public void connect(String host, Authentication auth) 
             throws ProtocolException {
         try {
-            ftp = FTPConnectionFactory.getInstance(host, 21, username,
-                    password, null, 10000, FTPConnection.FTP_CONNECTION, true);
+            ftp = FTPConnectionFactory.getInstance(host, 21, auth.getUser(),
+    		   auth.getPass(), null, 10000, FTPConnection.FTP_CONNECTION, true);
             ftp.connect();
         } catch (Exception e) {
             throw new ProtocolException("Failed to connect to " + host + " : "
@@ -84,37 +89,33 @@ public class Ftp4CheFtpClient extends Protocol {
     }
 
     @Override
-    public void disconnectFromServer() throws ProtocolException {
+    public void close() throws ProtocolException {
         ftp.disconnect();
     }
 
     @Override
-    public void getFile(ProtocolFile file, File toLocalFile)
+    public void get(ProtocolFile file, File toLocalFile)
             throws ProtocolException {
         try {
             ftp.downloadFile(
-                    new FTPFile(file.getProtocolPath().getPathString(), file
-                            .isDirectory()), new FTPFile(toLocalFile));
+                    new FTPFile(file.getPath(), file
+                            .isDir()), new FTPFile(toLocalFile));
         } catch (Exception e) {
             throw new ProtocolException("Failed to download " + file + " : "
                     + e.getMessage());
         }
     }
 
-    public void abortCurFileTransfer() {
-        ftp.disconnect();
-    }
-
-    public List<ProtocolFile> listFiles() throws ProtocolException {
+    public List<ProtocolFile> ls() throws ProtocolException {
         try {
             List<FTPFile> fileList = ftp.getDirectoryListing();
             List<ProtocolFile> returnList = new LinkedList<ProtocolFile>();
             for (int i = 0; i < fileList.size(); i++) {
                 FTPFile file = fileList.get(i);
-                String path = this.pwd().getProtocolPath().getPathString();
-                returnList.add(new ProtocolFile(this.getRemoteSite(),
-                        new ProtocolPath(path + "/" + file.getName(), file
-                                .isDirectory())));
+                String path = this.pwd().getPath();
+                ProtocolFile pFile = new ProtocolFile(path + "/" + file.getName(), 
+						      file.isDirectory());
+                returnList.add(pFile);
             }
             return returnList;
         } catch (Exception e) {
@@ -124,15 +125,46 @@ public class Ftp4CheFtpClient extends Protocol {
     }
 
     @Override
-    public boolean isConnected() throws ProtocolException {
-        return ftp.getConnectionStatus() == ftp.CONNECTED;
+    public List<ProtocolFile> ls(ProtocolFileFilter filter) throws ProtocolException{
+        try {
+            List<FTPFile> fileList = ftp.getDirectoryListing();
+            List<ProtocolFile> returnList = new LinkedList<ProtocolFile>();
+            for (int i = 0; i < fileList.size(); i++) {
+                FTPFile file = fileList.get(i);
+                String path = this.pwd().getPath();
+                ProtocolFile pFile = new ProtocolFile(path + "/" + file.getName(), file
+						      .isDirectory());
+                if(filter.accept(pFile)) returnList.add(pFile);
+            }
+            return returnList;
+        } catch (Exception e) {
+            throw new ProtocolException("Failed to get file list : "
+					+ e.getMessage());
+        }
+
+
     }
 
     @Override
-    public ProtocolFile getCurrentWorkingDir() throws ProtocolException {
+	public boolean connected(){
+        return ftp.getConnectionStatus() == ftp.CONNECTED;
+    }
+
+
+    @Override
+    public void put(File fromFile, ProtocolFile toFile) throws ProtocolException {
+	try {
+	    ftp.uploadFile(new FTPFile(fromFile.getAbsolutePath(),fromFile.getName(), fromFile.isDirectory()), new FTPFile(toFile.getPath(), toFile.getName(), toFile.isDir()));
+	}catch (Exception e) {
+	    throw new ProtocolException("Failed to put file '" + fromFile + "' : " + e.getMessage(), e);
+	}
+    }
+
+    @Override
+    public ProtocolFile pwd() throws ProtocolException {
         try {
-            return new ProtocolFile(this.getRemoteSite(), new ProtocolPath(ftp
-                    .getWorkDirectory(), true));
+            return new ProtocolFile(ftp
+				    .getWorkDirectory(), true);
         } catch (Exception e) {
             throw new ProtocolException("pwd command failed : "
                     + e.getMessage());
@@ -140,13 +172,12 @@ public class Ftp4CheFtpClient extends Protocol {
     }
 
     @Override
-    protected boolean deleteFile(ProtocolFile file) {
+    public void delete(ProtocolFile file) throws ProtocolException{
         try {
-            ftp.deleteFile(new FTPFile(file.getProtocolPath().getPathString(),
+            ftp.deleteFile(new FTPFile(file.getPath(),
                     false));
-            return true;
         } catch (Exception e) {
-            return false;
+            throw new ProtocolException(e.getMessage());
         }
     }
 
